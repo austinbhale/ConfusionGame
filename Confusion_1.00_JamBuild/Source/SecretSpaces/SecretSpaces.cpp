9017ce77828d438c7b8176ce541b3d085588a28b
Austin Hale <32556981+austinbhale@users.noreply.github.com>
2020-08-05 21:24:13 -0400

    Testing all but Content

---
+++ Confusion_1.00_JamBuild/Source/SecretSpaces/SecretSpaces.cpp
@@ -0,0 +1,6 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#include "SecretSpaces.h"
+#include "Modules/ModuleManager.h"
+
+IMPLEMENT_PRIMARY_GAME_MODULE( FDefaultGameModuleImpl, SecretSpaces, "SecretSpaces" );
---
+++ Confusion_1.00_JamBuild/Source/SecretSpaces/SecretSpacesGameModeBase.h
@@ -0,0 +1,17 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameModeBase.h"
+#include "SecretSpacesGameModeBase.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class SECRETSPACES_API ASecretSpacesGameModeBase : public AGameModeBase
+{
+	GENERATED_BODY()
+	
+};
